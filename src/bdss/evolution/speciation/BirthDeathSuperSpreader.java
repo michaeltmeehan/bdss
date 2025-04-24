50444d388ab22247d87f9ef16be573d3b452aceb
michaeltmeehan <michael.meehan1@jcu.edu.au>
2025-04-24 11:22:45 +1000

    Stopped overwriting input params

--- src/bdss/evolution/speciation/BirthDeathSuperSpreader.java
+++ src/bdss/evolution/speciation/BirthDeathSuperSpreader.java
@@ -1,78 +1,105 @@
 package bdss.evolution.speciation;
+
 import beast.base.core.Input;
 import beast.base.inference.parameter.RealParameter;
 import bdmm.evolution.speciation.BirthDeathMigrationModelUncoloured;
 import java.util.ArrayList;
 import java.util.Arrays;
+import beast.base.evolution.tree.TreeInterface;
+import beast.base.evolution.tree.Node;
 
-public class BirthDeathSuperSpreader extends BirthDeathMigrationModelUncoloured{
-	
-	static {
-        System.err.println(">>> BirthDeathSuperSpreader class loaded.");
+public class BirthDeathSuperSpreader extends BirthDeathMigrationModelUncoloured {
+
+    public Input<RealParameter> relative_transmissibility =
+            new Input<>("relative_transmissibility", "Relative transmissibility", (RealParameter) null);
+
+    public Input<RealParameter> total_r0 =
+            new Input<>("total_r0", "Total R0", (RealParameter) null);
+
+    public Input<RealParameter> superspreader_fraction =
+            new Input<>("superspreader_fraction", "Superspreader fraction", (RealParameter) null);
+
+    @Override
+    public void initAndValidate() {
+		// !!! Apparently I can't overwrite inherited inputs - it breaks BEAUti (but not BEAST) //
+        // Initialize inherited fields that must not be null
+        /*R0 = new Input<>("R0", "The basic reproduction number", new RealParameter(new Double[]{0.0, 0.0}));
+        R0AmongDemes = new Input<>("R0AmongDemes", "Among-deme R0", new RealParameter(new Double[]{0.0, 0.0}));
+        migrationMatrix = new Input<>("migrationMatrix", "Migration matrix", new RealParameter(new Double[]{0.0, 0.0}));
+
+        // Auto-generate empty tiptypes if missing
+        if (tiptypes.get() != null && (tiptypes.get().traitsInput.get() == null || tiptypes.get().traitsInput.get().trim().isEmpty())) {
+            StringBuilder sb = new StringBuilder();
+            TreeInterface tree = treeInput.get();
+            for (Node node : tree.getExternalNodes()) {
+                sb.append(node.getID()).append("=NOT_SET,");
+            }
+            if (sb.length() > 0) {
+                sb.setLength(sb.length() - 1);
+            }
+            tiptypes.get().traitsInput.setValue(sb.toString(), tiptypes.get());
+            tiptypes.get().initAndValidate();
+        }*/
+
+        // Only transform if all required inputs are present
+        if (superspreader_fraction.get() != null &&
+            relative_transmissibility.get() != null &&
+            total_r0.get() != null) {
+            transform();
+        }
+
+        super.initAndValidate();
     }
-	
-	public Input<RealParameter> relative_transmissibility =
-				new Input<>("relative_transmissibility", "relative transmissibility", (RealParameter) null);
 
-	public Input<RealParameter> total_r0 =
-				new Input<>("total_r0", "total R0", (RealParameter) null);	
+    private void transform() {
+        try {
+            double c = superspreader_fraction.get().getValue();
+            double rho = relative_transmissibility.get().getValue();
+            double r = total_r0.get().getValue();
 
-	public Input<RealParameter> superspreader_fraction =
-				new Input<>("superspreader_fraction", "superspreader fraction", (RealParameter) null);	
-				
-	@Override
-	public void initAndValidate() {
-		System.out.println();
-		var starting_val_R0 = new RealParameter(new Double[]{0.0,0.0});
-		var starting_val_R0AmongDemes = new RealParameter(new Double[]{0.0,0.0});
-		R0 = new Input<RealParameter>("R0", "The basic reproduction number", starting_val_R0);
-		R0AmongDemes = new Input<RealParameter>("R0AmongDemes", "The basic reproduction number", starting_val_R0AmongDemes);
-		transform();
-		super.initAndValidate();
-		/*
-		System.out.println("Among demes");
-		for(var r0 : R0AmongDemes.get().getValues()){
-			System.out.println(r0);
-		}
-		System.out.println("R0s");	
-		for(var r0 : R0.get().getValues()){
-			System.out.println(r0);
-		}*/
-	}
-	private void transform(){
-		var c = superspreader_fraction.get().getValue();
-	 	var rho = relative_transmissibility.get().getValue();
-		var r = total_r0.get().getValue();
-		var denominator = 1. - (1. - rho) * (1. - c);
-		var r11 = c * r / denominator;
-		var r12 = (1. - c) * r / denominator;
-		var r21 = rho * r11;
-		var r22 = rho * r12;
-		R0.get().setValue(0, r11);
-		R0.get().setValue(1, r22);
-		R0AmongDemes.get().setValue(0, r12);
-		R0AmongDemes.get().setValue(1, r21);
-		var left = c * (r11 + r12) + (1. - c) * (r21 + r22);
-		var right = r11 + r22;
-		System.out.println(String.format("left %f", left));
-		System.out.println(String.format("right %f", right));
-		System.out.println(String.format("rtotal %f", r));
-		System.out.println(String.format("superspreader_fraction %f", c));
-		System.out.println(String.format("relative_transmissibility %f", rho));
-		System.out.println(String.format("r11 %f", r11));
-		System.out.println(String.format("r12 %f", r12));
-		System.out.println(String.format("r21 %f", r21));
-		System.out.println(String.format("r22 %f", r22));
-	}
-	@Override
-	protected Double updateRates(){
-		transform();
-		var res = super.updateRates();
-		/*System.out.print("bij ");
-		for(var value : b_ij){
-			System.out.print(String.format("%f ", value));
-		}
-		System.out.println();*/
-		return res;
-	}
+            double denominator = 1.0 - (1.0 - rho) * (1.0 - c);
+            double r11 = c * r / denominator;
+            double r12 = (1.0 - c) * r / denominator;
+            double r21 = rho * r11;
+            double r22 = rho * r12;
+
+            R0.get().setValue(0, r11);
+            R0.get().setValue(1, r22);
+            R0AmongDemes.get().setValue(0, r12);
+            R0AmongDemes.get().setValue(1, r21);
+
+            double left = c * (r11 + r12) + (1.0 - c) * (r21 + r22);
+            double right = r11 + r22;
+
+            System.out.printf("left %f%n", left);
+            System.out.printf("right %f%n", right);
+            System.out.printf("rtotal %f%n", r);
+            System.out.printf("superspreader_fraction %f%n", c);
+            System.out.printf("relative_transmissibility %f%n", rho);
+            System.out.printf("r11 %f%n", r11);
+            System.out.printf("r12 %f%n", r12);
+            System.out.printf("r21 %f%n", r21);
+            System.out.printf("r22 %f%n", r22);
+			Double[] r0vals = R0.get().getValues();
+			System.out.print("R0: ");
+			for (double v : r0vals) {
+			System.out.printf("%f ", v);
+			}
+			System.out.println();
+			Double[] r0offvals = R0AmongDemes.get().getValues();
+			System.out.print("R0AmongDemes: ");
+			for (double v : r0offvals) {
+			System.out.printf("%f ", v);
+			}
+			System.out.println();
+        } catch (Exception e) {
+            System.err.println("BirthDeathSuperSpreader transform error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected Double updateRates() {
+        transform(); // Safe because guarded above
+        return super.updateRates();
+    }
 }
